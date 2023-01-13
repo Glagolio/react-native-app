b528b9c408b69b12189289b6d86ec601b44daa11
Mykhailo Shyhin <103953002+Glagolio@users.noreply.github.com>
2023-01-13 20:05:45 +0200

    work with routers

--- App.js
+++ App.js
@@ -11,8 +11,20 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
 } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// import SCREENS
 import RegistrationScreen from "./screens/auth/RegistrationScreen";
 import LoginScreen from "./screens/auth/LoginScreen";
+import Home from "./screens/main/Home";
+import CommentsScreen from "./screens/main/CommentsScreen";
+import CreatePostScreen from "./screens/main/CreatePostScreen";
+import MapScreen from "./screens/main/MapScreen";
+import PostsScreen from "./screens/main/PostsScreen";
+import ProfileScreen from "./screens/main/ProfileScreen";
+
+const MainStack = createStackNavigator();
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
@@ -25,7 +37,7 @@ export default function App() {
           "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
         });
       } catch (err) {
-        console.warn(err.message);
+        console.log(err.message);
       } finally {
         setIsReady(true);
       }
@@ -38,42 +50,11 @@ export default function App() {
   }
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <ImageBackground
-          style={styles.backgroundImage}
-          source={require("./assets/photoBG.jpg")}
-        >
-          <KeyboardAvoidingView
-            style={styles.keyboard} // определяем ОС и настраиваем поведение клавиатуры
-            behavior={Platform.OS == "ios" ? "padding" : "height"}
-          >
-            <LoginScreen />
-          </KeyboardAvoidingView>
-        </ImageBackground>
-      </View>
-    </TouchableWithoutFeedback>
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName="Login">
+        <MainStack.Screen name="Register" component={RegistrationScreen} />
+        <MainStack.Screen name="Login" component={LoginScreen} />
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // alignItems: 'center',
-    backgroundColor: "#000",
-    // justifyContent: 'center',
-  },
-  backgroundImage: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-  },
-  text: {
-    color: "#fff",
-  },
-  keyboard: {
-    width: "100%",
-  },
-});
--- screens/auth/RegistrationScreen.js
+++ screens/auth/RegistrationScreen.js
@@ -8,9 +8,11 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Keyboard,
+  ImageBackground,
+  TouchableWithoutFeedback,
 } from "react-native";
 
-const RegistrationScreen = () => {
+const RegistrationScreen = ({ navigation }) => {
   const [hidenPassword, setHidePassword] = useState({
     hide: true,
     text: "Show",
@@ -55,66 +57,101 @@ const RegistrationScreen = () => {
   };
 
   return (
-    <KeyboardAvoidingView style={styles.registrationForm}>
-      <Image
-        style={styles.avatar}
-        source={require("../../assets/defaultAvatar1.jpg")}
-      />
-      <Text style={styles.text}>Реєстрація</Text>
-      <TextInput
-        style={styles.input__login}
-        placeholder={"Login"}
-        onFocus={() => setIsShowKeyboard(true)}
-        onChangeText={setLogin}
-        value={login}
-      />
-      <TextInput
-        style={styles.input__login}
-        placeholder={"Enter the email"}
-        dataDetectorTypes={"address"}
-        onChangeText={setEmail}
-        value={email}
-      />
-      <View
-        style={{ ...styles.password, marginBottom: isShowKeyboard ? 32 : 0 }}
-      >
-        <TextInput
-          style={styles.input__login}
-          secureTextEntry={hidenPassword.hide}
-          dataDetectorTypes={"all"}
-          placeholder={"Enter the password"}
-          onChangeText={setPassword}
-          value={password}
-        />
-        <TouchableOpacity
-          style={styles.showPasswordButton}
-          onPress={hidePassword}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.backgroundImage}
+          source={require("../../assets/photoBG.jpg")}
         >
-          <Text style={styles.showPasswordButton__text}>
-            {hidenPassword.text}
-          </Text>
-        </TouchableOpacity>
-      </View>
-      {!isShowKeyboard && (
-        <View style={styles.buttons}>
-          <TouchableOpacity
-            style={styles.registrationButton}
-            onPress={handleSubmit}
+          <KeyboardAvoidingView
+            style={styles.keyboard}
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
           >
-            <Text style={styles.registrationButton__text}>Зареєструватись</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.haveAccountButton}>
-            <Text style={styles.haveAccountButton__text}>
-              Вже є аккаунт? Увійти
-            </Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </KeyboardAvoidingView>
+            <View style={styles.registrationForm}>
+              <Image
+                style={styles.avatar}
+                source={require("../../assets/defaultAvatar1.jpg")}
+              />
+              <Text style={styles.text}>Реєстрація</Text>
+              <TextInput
+                style={styles.input__login}
+                placeholder={"Login"}
+                onFocus={() => setIsShowKeyboard(true)}
+                onChangeText={setLogin}
+                value={login}
+              />
+              <TextInput
+                style={styles.input__login}
+                placeholder={"Enter the email"}
+                dataDetectorTypes={"address"}
+                onChangeText={setEmail}
+                value={email}
+              />
+              <View
+                style={{
+                  ...styles.password,
+                  marginBottom: isShowKeyboard ? 32 : 0,
+                }}
+              >
+                <TextInput
+                  style={styles.input__login}
+                  secureTextEntry={hidenPassword.hide}
+                  dataDetectorTypes={"all"}
+                  placeholder={"Enter the password"}
+                  onChangeText={setPassword}
+                  value={password}
+                />
+                <TouchableOpacity
+                  style={styles.showPasswordButton}
+                  onPress={hidePassword}
+                >
+                  <Text style={styles.showPasswordButton__text}>
+                    {hidenPassword.text}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              {!isShowKeyboard && (
+                <View style={styles.buttons}>
+                  <TouchableOpacity
+                    style={styles.registrationButton}
+                    onPress={handleSubmit}
+                  >
+                    <Text style={styles.registrationButton__text}>
+                      Зареєструватись
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.haveAccountButton}>
+                    <Text
+                      style={styles.haveAccountButton__text}
+                      onPress={() => navigation.navigate("Login")}
+                    >
+                      Вже є аккаунт? Увійти
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+    backgroundColor: "#000",
+    // justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
   text: {
     marginTop: 92,
     textAlign: "center",
@@ -226,6 +263,9 @@ const styles = StyleSheet.create({
   },
   buttons: {
     alignItems: "center",
+  },
+  keyboard: {
+    width: "100%",
   },
 });
 
