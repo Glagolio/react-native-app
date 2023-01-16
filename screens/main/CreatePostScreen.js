b7200cb3469be670a6084f706fad04018455dee9
Mykhailo Shyhin <103953002+Glagolio@users.noreply.github.com>
2023-01-16 12:12:45 +0200

    work with Camera

--- screens/main/CreatePostScreen.js
+++ screens/main/CreatePostScreen.js
@@ -1,12 +1,81 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const CreatePostScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>Create Post Screen</Text>
+    <View style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        ref={(ref) => {
+          setCameraRef(ref);
+        }}
+      >
+        {/* <Image styles={styles.preview} source={} /> */}
+        <TouchableOpacity
+          style={styles.snap}
+          onPress={async () => {
+            if (cameraRef) {
+              const { uri } = await cameraRef.takePictureAsync();
+              console.log(cameraRef);
+              await MediaLibrary.createAssetAsync(uri);
+            }
+          }}
+        >
+          <MaterialIcons name="photo-camera" size={24} color="black" />
+        </TouchableOpacity>
+      </Camera>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+    backgroundColor: "#fff",
+    // justifyContent: 'center',
+  },
+  camera: {
+    marginHorizontal: 16,
+    marginTop: 32,
+    height: 240,
+    position: "relative",
+  },
+  snap: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -50 }, { translateY: -50 }],
+  },
+});
 
 export default CreatePostScreen;
--- screens/main/PostsScreen.js
+++ screens/main/PostsScreen.js
@@ -1,12 +1,51 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const PostsScreen = () => {
   return (
-    <View>
-      <Text>Posts Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.userInfo}>
+        <Image
+          source={require("../../assets/defaultAvatar1.jpg")}
+          style={styles.avatar}
+        />
+        <View style={{ marginLeft: 8 }}>
+          <Text style={styles.name}>Natali Romanova</Text>
+          <Text style={styles.email}>email@example.com</Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: 'center',
+    backgroundColor: "#fff",
+    // justifyContent: 'center',
+  },
+  userInfo: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 32,
+    marginLeft: 16,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+  },
+  name: {
+    fontSize: 13,
+    lineHeight: 15,
+    fontFamily: "Roboto-Bold",
+  },
+  email: {
+    fontSize: 11,
+    lineHeight: 13,
+    color: "rgba(33, 33, 33, 0.8)",
+  },
+});
 
 export default PostsScreen;
